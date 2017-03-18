11a62e65e3d3b740980e63298e6e47cbdfab1076
Philipp Thurner <philipp_thurner@hotmail.com>
2017-03-18 20:47:38 +0000

    Update user fields

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
    def my_portfolio
-       
+       @stocks = current_user.stocks if current_user.stocks.any?
+       @user = current_user
    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,27 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :stocks
+  has_many :user_stocks
+  has_many :stocks, through: :user_stocks
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  def can_add_stock?(ticker_symbol)
+    under_stock_limit? && !stock_alredy_added?(ticker_symbol)
+  end
   
+  def under_stock_limit?
+    (user_stocks.count < 10)
+  end
+  
+  def stock_alredy_added?(ticker_symbol)
+    stock = Stock.find_by_ticker(ticker_symbol)
+    return false unless stock
+    user_stocks.where(stock_id: stock.id).exists?
+  end
+  
+  def full_name
+    return "#{first_name} #{last_name}".strip if (first_name || last_name)
+    "Anonumous"
+  end
 end
